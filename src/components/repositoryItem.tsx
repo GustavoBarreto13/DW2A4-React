9f27fb0b4b48346ece2a77795549096361163dc1
Gustavo Barreto <gustavobarreto1304@gmail.com>
2022-05-07 13:03:18 -0300

    20

---
+++ src/components/repositoryItem.tsx
@@ -0,0 +1,22 @@
+﻿interface repositoryItemProps {
+    repository: {
+        name: string;
+        description: string;
+        html_url: string;
+    }
+}
+
+export function repositoryItem(props: repositoryItemProps) {
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositórios</h1>
+            <ul>
+                <li>
+                    <strong>{props.repository?.name ?? 'Repositório Padrão'}</strong>
+                    <p>{props.repository?.description}</p>
+                    <a href={props.repository?.html_url}>Acesso ao Repositório</a>
+                </li>
+            </ul>
+        </section>
+    )
+}
---
+++ src/components/repositoryList.tsx
@@ -0,0 +1,36 @@
+﻿import '../styles/reposiries.scss';
+import { repositoryItem } from "./repositoryItem"
+import { useState, useEffect } from "react";
+import { CleanPlugin } from 'webpack';
+
+interface repository {
+    id : number;
+    name: string;
+    description: string;
+    html_url: string;
+}
+
+export function repositoryList() {
+
+    const [repositories, setRepositories] = useState<repository[]>([])
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/santicioli/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, [])
+
+
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositórios</h1>
+            <ul>
+                {repositories.map(repository => {
+                    return < repositoryItem repository={repository} key={repository.id}/>
+                })}
+                
+                
+            </ul>
+        </section>
+    )
+}
